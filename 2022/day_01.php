c53d8b98e35fe3234b3f875d6c1e37398ce3821d
marcguebel <contact@guebel-marc.fr>
2022-12-01 18:07:25 +0100

    start 2022 day 1

---
+++ 2022/day_01.php
@@ -0,0 +1,33 @@
+<?php
+
+// challenge see : https://adventofcode.com/2022/day/1
+
+$input = explode("\r\n", file_get_contents(__DIR__ . '/day_01_input.txt') );
+
+// 1st
+$max = 0;
+$current_sum = 0;
+foreach ($input as $line) {
+	if ($line != '') {
+		$current_sum += (int) $line;
+	} else {
+		if ($current_sum > $max)
+			$max = $current_sum;
+		$current_sum = 0;
+	}
+}
+echo 'Answer 1 : ' . $max . '<br />';
+
+
+// 2nd
+$sums = [0];
+foreach ($input as $line) {
+	if ($line == '') {
+		$sums[] = 0;
+	} else {
+		$sums[sizeof($sums) -1] += $line;
+	}
+}
+sort($sums);
+
+echo 'Answer 2 : ' . ($sums[sizeof($sums) -3] + $sums[sizeof($sums) -2] + max($sums) );
--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # advent_of_code
-Calendrier de l'avent pour développeur web, mes solutions
 
-Le projet regroupe les solutions que je propose pour les défis du calendrier de l'avent adventofcode disponible à l'adresse : https://adventofcode.com/2021
+See : https://adventofcode.com/
+Thats my solution for aoc 
+
+Im a Junior developper and I test myself with this programming puzzles 
+
+You can found my 2021 & 2022 solution
