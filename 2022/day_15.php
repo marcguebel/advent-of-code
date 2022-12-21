29c9ffb3149ef3278c1d1107a673fa95510acdea
marcguebel <contact@guebel-marc.fr>
2022-12-21 22:27:57 +0100

    day 15 part 1

--- 2022/day_15.php
+++ 2022/day_15.php
@@ -1,13 +1,11 @@
 <?php
 // challenge see : https://adventofcode.com/2022/day/15
-$start = microtime(true);
-
 $input = explode("\r\n", file_get_contents(__DIR__ . '/day_15_input.txt') );
-echo '<pre>';
-require_once __DIR__.'/day_15_map.php';
 
 // 1st
-$map = new map();
+$targetLine = 2000000;
+$ranges = [];
+$beacons = [];
 foreach ($input as $line) {
 	$dataExplode = explode('=', $line);
 	$sensorColumn = explode(',', $dataExplode[1])[0];
@@ -15,16 +13,54 @@ foreach ($input as $line) {
 	$beaconColumn = explode(',', $dataExplode[3])[0];
 	$beaconLine = explode(':', $dataExplode[4])[0];
 
-	$map->addNode($sensorLine, $sensorColumn, 'S');
-	$map->addNode($beaconLine, $beaconColumn, 'B');
+	$range = abs($sensorColumn - $beaconColumn) + abs($sensorLine - $beaconLine);
+
+	//if reach the target line
+	if ($sensorLine < $targetLine && ($sensorLine + $range >= $targetLine)) {
+		$count = $range - ($targetLine - $sensorLine);
+		$ranges[] = ['start' => $sensorColumn - $count, 'end' => $sensorColumn + $count];
+	} elseif ($sensorLine > $targetLine && ($sensorLine - $range <= $targetLine)) {
+		$count = $targetLine - ($sensorLine - $range);
+		$ranges[] = ['start' => $sensorColumn - $count, 'end' => $sensorColumn + $count];
+	}
+	
+	//save beacons column on target line
+	if($beaconLine == $targetLine && !in_array($beaconColumn, $beacons)){
+		$beacons[] = $beaconColumn;
+	}
 }
-$map->addSignal();
-// $map->displayMap();
 
-echo microtime(true) - $start;
-// var_dump($map);
-// echo 'Answer 1 : ' . $map->runPartOne() . '<br />';
+sort($ranges);
 
+//merge range 
+$rangesMerge = [];
+$current = null;
+foreach($ranges as $index => &$range) {
+	if ($current == null) {
+		$current = $range;
+	} elseif($range['start'] <= $current['end']) {
+		if($range['end'] > $current['end']){
+			$current['end'] = $range['end'];
+		} 
+		unset($ranges[$index]);
+	} else {
+		$rangesMerge[] = $current;
+		$current = null;
+	}
+}
+$rangesMerge[] = $current;
+
+
+//outpout result with beacon positon deduction
+foreach($rangesMerge as $range) {
+	$countBeacon = 0;
+	foreach ($beacons as $beacon) {
+		if ($range['start'] <= $beacon && $range['end'] >= $beacon) {
+			$countBeacon++;
+		}
+	}
+	echo 'Answer 1 : '.(($range['end'] - $range['start'] +1 ) - $countBeacon) . '<br />';
+}
 // 2nd
 
 // echo 'Answer 2 : ' . $map->runPartTwo() . '<br />';
