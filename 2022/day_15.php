f947427aa7085642e0a459c23ecbc7616d3148da
marcguebel <contact@guebel-marc.fr>
2022-12-23 18:19:54 +0100

    day 15

--- 2022/day_15.php
+++ 2022/day_15.php
@@ -3,7 +3,7 @@
 $input = explode("\r\n", file_get_contents(__DIR__ . '/day_15_input.txt') );
 
 // 1st
-$targetLine = 2000000;
+$targetLine = 10;
 $ranges = [];
 $beacons = [];
 foreach ($input as $line) {
@@ -50,7 +50,6 @@ foreach($ranges as $index => &$range) {
 }
 $rangesMerge[] = $current;
 
-
 //outpout result with beacon positon deduction
 foreach($rangesMerge as $range) {
 	$countBeacon = 0;
@@ -61,6 +60,59 @@ foreach($rangesMerge as $range) {
 	}
 	echo 'Answer 1 : '.(($range['end'] - $range['start'] +1 ) - $countBeacon) . '<br />';
 }
-// 2nd
 
-// echo 'Answer 2 : ' . $map->runPartTwo() . '<br />';
+// 2nd
+for ($i=0; $i < 4000000; $i++) { 
+	$targetLine = $i;
+	$ranges = [];
+	$beacons = [];
+	foreach ($input as $line) {
+		$dataExplode = explode('=', $line);
+		$sensorColumn = explode(',', $dataExplode[1])[0];
+		$sensorLine = explode(':', $dataExplode[2])[0];
+		$beaconColumn = explode(',', $dataExplode[3])[0];
+		$beaconLine = explode(':', $dataExplode[4])[0];
+
+		$range = abs($sensorColumn - $beaconColumn) + abs($sensorLine - $beaconLine);
+
+		//if reach the target line
+		if ($sensorLine <= $targetLine && ($sensorLine + $range > $targetLine)) {
+			$count = $range - ($targetLine - $sensorLine);
+			$ranges[] = ['start' => $sensorColumn - $count, 'end' => $sensorColumn + $count];
+		} elseif ($sensorLine >= $targetLine && ($sensorLine - $range < $targetLine)) {
+			$count = $targetLine - ($sensorLine - $range);
+			$ranges[] = ['start' => $sensorColumn - $count, 'end' => $sensorColumn + $count];
+		}
+		
+		//save beacons column on target line
+		if($beaconLine == $targetLine && !in_array($beaconColumn, $beacons)){
+			$beacons[] = $beaconColumn;
+		}
+	}
+
+	sort($ranges);
+
+	//merge range 
+	$rangesMerge = [];
+	$current = $ranges[0];
+	foreach($ranges as $index => &$range) {
+		if (!isset($current['end'])) {
+			$current['end'] = $range['end'];
+		} elseif($range['start'] <= $current['end']+1) {
+			if($range['end'] > $current['end']){
+				$current['end'] = $range['end'];
+			} 
+			unset($ranges[$index]);
+		} else {
+			$rangesMerge[] = $current;
+			$current['start'] = $range['start'];
+			$current['end'] = null;
+		}
+	}
+	$rangesMerge[] = $current;
+
+	if(count($rangesMerge)>1){
+		echo 'Answer 2 : ' . (($rangesMerge[0]['end']+1) * 4000000) + $i . '<br />';
+		exit;
+	}	
+}
